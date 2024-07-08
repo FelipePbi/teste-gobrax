dbc336eb9af598ea045184410ef8fda94169961a
Felipe Borges <felipe.rosa@grupoboticario.com.br>
2024-07-08 04:41:06 -0300

    refactor: add global types

---
+++ src/types/global.ts
@@ -0,0 +1,12 @@
+export type Driver = {
+  id: number;
+  name: string;
+  document: string;
+  vehicleId: number;
+};
+
+export type Vehicle = {
+  id: number;
+  brand: string;
+  plate: string;
+};
