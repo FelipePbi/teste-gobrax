6a8d832bd1b99d266e9cdcee7df4c5aae53b6b60
Felipe Borges <felipe.rosa@grupoboticario.com.br>
2024-07-08 03:09:42 -0300

    refactor: add modals and hooks in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,14 +3,41 @@ import Header from "../../components/Header/Header";
 import SelectedRowInfo from "../../components/SelectedRowInfo/SelectedRowInfo";
 import "./Home.scss";
 import { Container } from "@mui/material";
+import DriverModal from "../../components/DriverModal/DriverModal";
+import { useDriverHook } from "../../hooks/useDriverHook";
+import { useMemo } from "react";
+import type { MenuOptions } from "../../components/Header/types";
+import { useVehicleHook } from "../../hooks/useVehicleHook";
+import VehicleModal from "../../components/VehicleModal/VehicleModal";
 
 function Home() {
+  const driverHook = useDriverHook();
+  const vehicleHook = useVehicleHook();
+
+  const optionsMenuHeader: MenuOptions[] = useMemo(() => {
+    return [
+      {
+        label: "Motoristas",
+        onClick: driverHook.onOpenModal,
+      },
+      {
+        label: "Veículos",
+        onClick: vehicleHook.onOpenModal,
+      },
+    ];
+  }, [driverHook.onOpenModal, vehicleHook.onOpenModal]);
+
   return (
-    <Container className="home-container">
-      <Header />
-      <SelectedRowInfo />
-      <Grid />
-    </Container>
+    <>
+      <Container className="home-container">
+        <Header menuOptions={optionsMenuHeader} />
+        <SelectedRowInfo />
+        <Grid />
+      </Container>
+
+      <VehicleModal {...vehicleHook} />
+      <DriverModal {...driverHook} />
+    </>
   );
 }
 
