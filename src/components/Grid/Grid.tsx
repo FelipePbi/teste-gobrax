0c0a39daef94479936256252d12da375419eb084
Felipe Borges <felipe.rosa@grupoboticario.com.br>
2024-07-08 14:22:49 -0300

    refactor: update grid component

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -3,10 +3,29 @@ import "./Grid.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { columns } from "./constants";
 import { useGridHook } from "../../hooks/useGridHook";
+import { Box, Typography } from "@mui/material";
+import { CoPresent } from "@mui/icons-material";
 
 function Grid() {
   const { handleSelectRow } = useSelectRowHook();
   const { gridLoading, gridRowData } = useGridHook();
+
+  if (!gridRowData?.length && !gridLoading) {
+    return (
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        paddingTop={18}
+      >
+        <CoPresent fontSize="large" />
+        <Typography variant="h5" component="h1" align="center" marginTop={3}>
+          Nenhum motorista adicionado!
+        </Typography>
+      </Box>
+    );
+  }
 
   return (
     <DataGrid
