22b3457b34f81dafbe839410472ed8e00bfd9ce8
Felipe Borges <felipe.rosa@grupoboticario.com.br>
2024-07-08 03:50:21 -0300

    feat: add provider in main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/Home/Home";
 import "./index.scss";
+import { SelectedRowProvider } from "./contexts/SelectedRowContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <SelectedRowProvider>
+      <App />
+    </SelectedRowProvider>
   </React.StrictMode>
 );
