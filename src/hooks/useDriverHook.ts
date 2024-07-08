c31a4d822b9d7e3b55a9f1b3021dbc3124ac4297
Felipe Borges <felipe.rosa@grupoboticario.com.br>
2024-07-08 04:42:13 -0300

    refactor: add api driver call

--- src/hooks/useDriverHook.ts
+++ src/hooks/useDriverHook.ts
@@ -1,6 +1,20 @@
+import { useQuery, useIsFetching } from "@tanstack/react-query";
 import { useCallback, useState } from "react";
+import { Driver } from "../types/global";
 
 export const useDriverHook = () => {
+  const isVehicleLoading = useIsFetching({ queryKey: ["vehicles"] });
+  const { data: driversData, isLoading: driversLoading } = useQuery({
+    queryKey: ["drivers"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3000/drivers");
+      const data: Driver[] = (await response.json()) as Driver[];
+
+      return data;
+    },
+    enabled: !!isVehicleLoading,
+  });
+
   const [openModal, setOpenModal] = useState(false);
 
   const onCloseModal = useCallback(() => {
@@ -17,9 +31,10 @@ export const useDriverHook = () => {
 
   return {
     openModal,
+    driversData,
+    driversLoading,
     onCloseModal,
     onOpenModal,
     onConfirmModal,
-    vehicleOptions: [],
   };
 };
